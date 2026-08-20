994e2afacb29f091f2dfadf987f675f443e11194
Lapatrada Kesornbua <kesornbua_l@silpakorn.edu>
2026-08-20 05:06:29 +0000

    labweek4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 /*function App() {
   const name = 'Lapatrada';
@@ -13,7 +13,7 @@ import './App.css';
     </div>
   );
 }*/
-import Header from './Components/Header';
+/*import Header from './Components/Header';
 import MovieList from './Components/MovieList';
 import Footer from './Components/Footer';
 import Greeting from './Components/Greeting';
@@ -30,7 +30,7 @@ function App() {
       <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
       <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
       </Card>
-      
+
        <h1>รายชื่อนักศึกษา</h1>
       {students.map((s) => (
         <Greeting key={s.id} name={s.name} year={s.year} />
@@ -39,6 +39,39 @@ function App() {
     </div>
   );
 }
+export default App;*/
 
+import ProfileCard from './Components/ProfileCard';
+import './App.css';
+
+const members = [
+  { id: 670710211, name: 'ลภัสรดา เกษรบัว', nickname: 'แพร',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาเขียว', 'แมว'] },
+
+  { id: 2, name: 'สมชาย สุดยอด', nickname: 'ชาย',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาไทย', 'หมา'] },
+
+  { id: 3, name: 'สมหญิง ใจดี', nickname: 'ฝน',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาเขียว', 'แมว'] },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <h1>สมาชิกกลุ่มของเรา</h1>
+      <div className="card-row">
+        {members.map((m) => (
+          <ProfileCard
+            key={m.id}
+            name={m.name}
+            nickname={m.nickname}
+            major={m.major}
+            favorites={m.favorites}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default App;
