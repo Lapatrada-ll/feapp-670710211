9b8a1c969a543fbe5cea5dda97f9bae6a7d5190e
Lapatrada Kesornbua <kesornbua_l@silpakorn.edu>
2026-09-10 03:40:31 +0000

    ready

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -39,7 +39,7 @@ function Navbar() {
       <button className="rounded-lg bg-cyan-600 px-4 py-2 font-semibold text-white
                    transition hover:bg-cyan-700 hover:shadow-lg
                    focus:outline-none focus:ring-2 focus:ring-cyan-400">
-  เข้าสู่ระบบ
+    เข้าสู่ระบบ
 </button>
       </div>
     </nav>
